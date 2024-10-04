bbb051e9fe08b19d5826859780965c9dce26da30
sina1010anis <sina1010anis@gmail.com>
2024-10-04 11:15:58 +0330

    fix

--- app/Http/Controllers/InsertDataController.php
+++ app/Http/Controllers/InsertDataController.php
@@ -10,29 +10,41 @@ class InsertDataController extends Controller
     public function elasticsearch ()
     {
 
-        $file = json_decode(file_get_contents(public_path('posts.json')), true);
+        try {
+            $file = json_decode(file_get_contents(public_path('posts.json')), true);
 
-        // return Http::put("http://127.0.0.1:9200/posts/");
+            // return Http::put("http://127.0.0.1:9200/posts/");
 
-        foreach ($file as $item) {
+            foreach ($file as $item) {
 
-            Http::put('http://127.0.0.1:9200/posts/_doc/'.$item['id'], [
+                Http::put('http://127.0.0.1:9200/posts/_doc/'.$item['id'], [
 
-                'title' => $item['title'],
+                    'title' => $item['title'],
 
-                'descrtion' => $item['descrtion'],
+                    'descrtion' => $item['descrtion'],
 
-                'download' => $item['download'],
+                    'download' => $item['download'],
 
-                'view' => $item['view'],
+                    'view' => $item['view'],
 
-                'keyword' => $item['keyword'],
+                    // 'keyword' => str_replace('/', '', $item['keyword']),
 
-                'created_at' => $item['created_at']
+                    'keyword' => json_decode($item['keyword']),
 
-            ]);
+                    'created_at' => $item['created_at']
+
+                ]);
+            }
+
+            return "Inserted ...!";
+
+        } catch (\Exception $e) {
+
+            return $e->getMessage();
 
         }
+
+
 
 
     }
