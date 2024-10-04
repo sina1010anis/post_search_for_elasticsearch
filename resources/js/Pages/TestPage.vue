1a251ee141b6db6fada24c0c3ca88b26c38ab653
sina1010anis <sina1010anis@gmail.com>
2024-10-04 17:08:41 +0330

    install vuetify

---
+++ resources/js/Pages/TestPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Hello world !!!</h1>
+        <v-btn color="primary">Button</v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestPage',
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
+import { components } from 'vuetify/dist/vuetify-labs.js';
 import './bootstrap';
+import {createApp} from 'vue/dist/vue.esm-bundler';
+import TestPage from "./Pages/TestPage.vue"
+import vuetify from "./vuetify";
+
+
+const app = createApp({
+    data :() => ({
+        version : "vuejs 3v"
+    }),
+    components: {
+        TestPage:TestPage
+    }
+})
+
+
+app.use(vuetify).mount("#app")
